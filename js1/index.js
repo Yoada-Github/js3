ecabc82be4c4e53edbf5d5f645fcad84e0bb332b
Yoseph Adane <yosephadane63@Gmail.com>
2024-10-08 14:18:25 +0300

    Add files via upload

---
+++ js1/index.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(num1,num2) {
+    if (num1===num2) {
+        return 3*(num1+num2);
+    } else {
+        return (num1 + num2);
+    }
+    
+ 
+}
+console.log(addTwoNumbers(6,6));
+console.log(addTwoNumbers(17,6));
